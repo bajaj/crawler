e4e10386bd2065721d23cb18e345501e27818a78
Ritesh Bajaj <riteshr.bajaj@swiggy.in>
2017-03-20 03:21:45 +0530

    page request queue test

---
+++ src/test/java/EventQueue/PageRequestQueueTest.java
@@ -0,0 +1,56 @@
+package EventQueue;
+
+import org.junit.Test;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by bajaj on 20/03/17.
+ */
+public class PageRequestQueueTest {
+
+    @Test
+    public void testIsEmpty() throws Exception {
+        PageRequestQueue pageRequestQueue = new PageRequestQueue();
+        assertEquals("queue should be empty", true, pageRequestQueue.isEmpty());
+
+        pageRequestQueue.add(new URL("https://gocardless.com/"));
+        assertEquals("queue should not be empty", true, pageRequestQueue.isEmpty());
+
+        pageRequestQueue.remove();
+        assertEquals("queue should be empty", false, pageRequestQueue.isEmpty());
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+        PageRequestQueue pageRequestQueue = new PageRequestQueue();
+        URL url = new URL("https://gocardless.com/");
+        pageRequestQueue.add(url);
+        assertEquals("Returned object from remove call", url, pageRequestQueue.remove());
+    }
+
+    @Test
+    public void testAddAll() throws Exception {
+        PageRequestQueue pageRequestQueue = new PageRequestQueue();
+        URL url1 = new URL("https://gocardless.com/");
+        pageRequestQueue.add(url1);
+
+        URL url2 = new URL("https://gocardless.com/support/");
+        pageRequestQueue.add(url2);
+
+        assertEquals("Returned object from 1st remove call", url1, pageRequestQueue.remove());
+        assertEquals("Returned object from 2nd remove call", url2, pageRequestQueue.remove());
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        PageRequestQueue pageRequestQueue = new PageRequestQueue();
+        URL url1 = new URL("https://gocardless.com/");
+        pageRequestQueue.add(url1);
+        assertEquals("Returned object from 1st remove call", url1, pageRequestQueue.remove());
+    }
+
+}
